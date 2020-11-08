0468ac20b6c0ee4b63cccb21213dd563395db7d1
Germán Lugo <german.lugo@bitnibs.com>
2020-11-08 21:51:04 +0100

    Initial commit.

---
+++ 2.Primitives/literal_and_operators.rs
@@ -0,0 +1,22 @@
+fn main() {
+    // Integer addition
+    println!("1 + 2 = {}", 1u32 + 2);
+
+    // Integer subtraction
+    println!("1 - 2 = {}", 1i32 - 2);
+
+    // Short-circuiting boolean logic
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+
+    // Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 32  is {}", 1u64 << 32);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    // Use underscores to improve readability!
+    println!("Ten million is written as {}", 10_000_000u32);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+My notes while I study Rust 🦀.
